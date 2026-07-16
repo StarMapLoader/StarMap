9eb2d8c2109c815bb2d5ee282e0437c2576e87a0
Jude Nimo <judenimo@users.noreply.github.com>
2026-07-16 07:35:35 +0100

    Fixed runtime path #1

--- StarMap.Loader/Program.cs
+++ StarMap.Loader/Program.cs
@@ -32,7 +32,7 @@ namespace StarMap
                 return;
             }
 
-            AssemblyLoadContext.Default.LoadFromAssemblyPath(Path.GetFullPath("./0Harmony.dll"));
+            AssemblyLoadContext.Default.LoadFromAssemblyPath(Path.Combine(AppContext.BaseDirectory,"0Harmony.dll"));
 
             var gameAssemblyContext = new GameAssemblyLoadContext(gameConfig.GameLocation);
             var dumbFacade = new SoloGameFacade();
